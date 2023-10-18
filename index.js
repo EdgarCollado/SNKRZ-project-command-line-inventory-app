43d4cded7b93ed1b273086dbde1196f6d53a1adb
EdgarCollado <edgarcollado@pursuit.org>
2023-10-18 16:08:13 -0400

    updated-index.js-functions

--- index.js
+++ index.js
@@ -32,3 +32,49 @@ function generateProducts() {
 
     return brandsInventory;
 }
+
+function processInput() {
+    const expectedCommand = process.argv[2];
+    let result = "Error: Command not found";
+
+    if (expectedCommand === "create") {
+        const [name, price, stock] = process.argv.slice(3);
+        const parsedPrice = parseFloat(price);
+        const parsedStock = parseInt(stock);
+        result = productsAPI.createBakeryItem(name, parsedPrice, parsedStock);
+    } else if (expectedCommand === "list") {
+        result = productsAPI.listBakeryItems();
+    } else if (expectedCommand === "view") {
+        const index = parseInt(process.argv[3]);
+        result = productsAPI.getBakeryItemDetails(index);
+    } else if (expectedCommand === "delete") {
+        const index = parseInt(process.argv[3]);
+        result = productsAPI.deleteBakeryItem(index);
+    } else if (expectedCommand === "update") {
+        const index = parseInt(process.argv[3]);
+        const [name, price, stock] = process.argv.slice(4);
+        const parsedPrice = parseFloat(price);
+        const parsedStock = parseInt(stock);
+        result = productsAPI.updateBakeryItem(index, name, parsedPrice, parsedStock);
+    } else if (expectedCommand === "generate") {
+        const bakedItems = generateBakedGoods();
+        fs.writeFileSync(dataFilePath, JSON.stringify(bakedItems, null, 2));
+        result = "Random items generated and saved to bakery.json";
+    } else if (expectedCommand === "addToCart") {
+        productsAPI.addToCart();
+        result = "Item added to the cart.";
+    } else if (expectedCommand === "view-cart") {
+        productsAPI.viewCart();
+        result = "Shopping Cart:";
+    } else if (expectedCommand === "calculate-total") {
+        const totalPrice = productsAPI.calculateTotalPrice();
+        result = `Total Price: $${totalPrice.toFixed(2)}`;
+    } else if (expectedCommand === "clear-cart") {
+        productsAPI.clearCart();
+        result = "Cart cleared.";
+    }
+
+    console.log(result);
+}
+
+processInput();
